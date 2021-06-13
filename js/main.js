e9f8014ab0568097e76dec43a83c9e06901b23a0
Rinakae <simi1993@mail.ru>
2021-06-13 21:10:03 +0300

    create: added phpmailer

--- js/main.js
+++ js/main.js
@@ -34,6 +34,5 @@ keyboard: {
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
-console.log("клик по кнопке меню");
 document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom__visible");
 });
